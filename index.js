ac7ae6796f0142d5a308d95c6c3f95233a4bd058
May Chen <maychen@Mays-MacBook-Pro.local>
2019-08-13 22:30:16 -1000

    Added billing and client side app

--- index.js
+++ index.js
@@ -41,6 +41,21 @@ app.use(passport.session());
 require('./routes/authRoutes.js')(app);
 require('./routes/billingRoutes.js')(app);
 
+//make sure express behaves currently when in the production environment
+if (process.env.NODE_ENV === 'production') {
+  //Express will serve up production assets like main.js or main.css
+  //If any get request for some route that we don't understand the route, then look into the client/build directory and see if there is some file in the directory that matches up with what the request is looking for and if so, respond with that file
+  app.use(express.static('client/build'));
+
+  //Express will serve up the index.html file if it doesn't recognize the route
+  //if someone makes a request that we don't understand then serve up index.html
+  //This is 2nd option because of order of operations, the "catch all" option
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 /* Instructs express to tell node (the runtime) to listen to PORT
     - Whenever Heroku runs our application, Heroku has ability to inject environment variables. Environemnet vairables are variables that are set in underlying runtime that node is running on top of. Heroku's opportunity to pass us runtime configurations that Heroku only wants to tell us only after it has deployed the application
     - If running in production then can use 'process.env.PORT'
