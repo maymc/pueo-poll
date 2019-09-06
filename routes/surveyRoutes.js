0dcc1c3c4bf6b2560e70d62c5755b4e8925f59ab
May Chen <maychen@Mays-MacBook-Pro.local>
2019-09-05 20:38:23 -1000

    Able to gather feedback on surveys

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -11,26 +11,43 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
+
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for your input!');
   })
 
   app.post('/api/surveys/webhooks', (req, res) => {
     // console.log("webhook: ", req.body);
-    const events = _.map(req.body, ({ email, url }) => {
-      const pathname = new URL(url).pathname;
-      const p = new Path('/api/surveys/:surveyId/:choice'); //create a parser object to pull out the surveyID and choice. Path-parser helps parse out patterns
-      const match = p.test(pathname); //if there is a match, return match object found. If will return an object or null
-      if (match) {
-        return { email, surveyId: match.surveyId, choice: match.choice }
-      }
-    });
 
-    const compactEvents = _.compact(events);  //takes an array, goes through it, and removes anything 'undefined'
-    //go through list of events and look at email and survey id property for any duplicates. Compares both email and survey id.
-    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
-    console.log(uniqueEvents);
+    //create a parser object to pull out the surveyID and choice. Path-parser helps parse out patterns
+    const p = new Path('/api/surveys/:surveyId/:choice');
+    _.chain(req.body)  //iterate over req.body
+      .map(({ email, url }) => {
+
+        //if there is a match, return match object found. If will return an object or null
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice }
+        }
+      })
+      //takes an array, goes through it, and removes anything 'undefined'
+      .compact()
+      //go through list of events and look at email and survey id property for any duplicates. Compares both email and survey id.
+      .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne({
+          _id: surveyId,
+          recipients: {
+            $elemMatch: { email: email, responded: false }
+          }
+        }, {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
+          }).exec();  //execute to send to database
+      })
+      .value()
 
     res.send({});
   });
