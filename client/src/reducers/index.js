5aac534a00f44c580cf4d5c8855bf77fe45ea5ad
May Chen <maychen@Mays-MacBook-Pro.local>
2019-09-05 21:39:16 -1000

    Added CSS to make each survey a card

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,12 @@
 import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
+import surveysReducer from './surveysReducer';
 
 // important aspect of the combineReducers call - the object being passed in whatever keys we provide to the object are going to represent the keys that exist inside of our state
 //Want to name each key appropraitely
 export default combineReducers({
   auth: authReducer,
-  form: reduxForm
+  form: reduxForm,
+  surveys: surveysReducer
 });
